51fde675b68ca3bff94064115ddbc9684fe67c71
Kasthuri <krangan415@gmail.com>
2025-05-12 19:50:01 +0530

    updated middleware

--- middleware.js
+++ middleware.js
@@ -1,24 +1,22 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  // Check for the token in the cookies
-  const token = req.cookies.get('token');
-  console.log(token,"cleint token")
+  const token = req.cookies.get("token");
   const { pathname } = req.nextUrl;
 
   // Redirect logged-in users away from login/register pages
-  if (token && (pathname === '/auth/login' || pathname === '/auth/register')) {
-    return NextResponse.redirect(new URL('/', req.url));
+  if (token && (pathname === "/auth/login" || pathname === "/auth/register")) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   // Redirect logged-out users away from protected routes
-  if (!token && pathname !== '/auth/login' && pathname !== '/auth/register') {
-    return NextResponse.redirect(new URL('/auth/login', req.url));
+  if (!token && pathname !== "/auth/login" && pathname !== "/auth/register") {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/auth/login', '/auth/register', '/dashboard'], // Add more protected routes here
+  matcher: ["/auth/login", "/auth/register", "/", "/newTodo"], // Add protected routes
 };
